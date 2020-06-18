fa593d788d80769807ada93c19a5e622878dfd91
carolina <carolinaccez@gmail.com>
2020-06-18 17:55:41 -0400

    start and end game functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -80,7 +80,14 @@ var fight = function(enemyName) {
   }
 }; 
 
-for(var i = 0; i < enemyNames.length; i++) {
+var startGame = function() {
+// reset player stats
+playerHealth = 100;
+playerAttack = 10;
+playerMoney = 10;
+
+
+  for(var i = 0; i < enemyNames.length; i++) {
   if (playerHealth > 0) {
  
   // let user know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
@@ -104,7 +111,30 @@ for(var i = 0; i < enemyNames.length; i++) {
     window.alert("Your have lost your robot in battle! Game Over!");
     break;
   } 
+  
+ } 
+ 
+ endGame();
+};
+
+
+function endGame() {
+  // if player is still alive, player wins!
+  if (playerHealth > 0) {
+    window.alert("Great job, you've survived the game! You now have a score of " + playerMoney + ".");
+  }
+  else {
+    window.alert("You've lost your robot in battle.");
+  }
+
+var playAgainConfirm = window.confirm("Would you like to play again?");
+
+if (playAgainConfirm) {
+  // restart the game
+  startGame();
 } 
-
-
-
+else {
+  window.alert("Thank you for playing Robot Gladiators! Come back soon!");
+}
+  }
+  startGame();
