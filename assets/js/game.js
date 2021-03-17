09902a6e5eb5a10e47db6fb21b756b3a9da9d71f
carolina <carolinaccez@gmail.com>
2021-03-17 00:04:18 -0400

    added playerHealth and console log

--- assets/js/game.js
+++ assets/js/game.js
@@ -17,12 +17,14 @@ var fight = function() {
 
   //Log a resulting message to the console so we know that it worked. 
   console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has "+ enemyHealth + " health remaining."); 
-  
+
 
   //Subtract the value of 'enemyAttack' from the value of 'playerHealth 
-  //and use that result to update the value in the 'playerHealth' variable. 
+  //and use that result to update the value in the 'playerHealth' variable.
+  playerHealth = playerHealth - enemyAttack; 
 
-  //Log a resulting message to the console so we know that it worked. 
+  //Log a resulting message to the console so we know that it worked.
+  console.log(enemyName + " attacked" + playerName + ". " + playerName + " now has " +playerHealth+ " health remaining.") 
 }; 
 
 fight(); 
