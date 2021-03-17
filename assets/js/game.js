abed4a46ac103ed365ba12da52652cea6c8592b6
carolina <carolinaccez@gmail.com>
2021-03-16 23:13:11 -0400

    player and enemy, health, attack and name

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,31 @@
+var playerName = window.prompt("What is your robot's name?"); 
+var playerHealth = 100; 
+var playerAttack = 10; 
 
-var playerName = window.prompt("What is your robot's name?")
-console.log(playerName);
+console.log(playerName, playerAttack, playerHealth); 
 
-console.log("This logs a string, good for leaving yourself a message"); 
-console.log(10 + 10); 
-console.log("Our robot's name is " + playerName); 
+var enemyName = "Roborto"; 
+var enemyHealth = 50; 
+var enemyAttack = 12; 
 
-// this creates a function named "fight"
+var fight = function() { 
+  window.alert("Welcome to Robot Gladiators!");
 
-function fight() { 
-  window.alert("The fight has begun!");
-}
+  //Subtract the value of 'playerAttack' from the value of 'enemyHealth' 
+  //and use that result to update the value in the 'enemyHealth' variable. 
+  enemyHealth = enemyHealth - playerAttack; 
 
-// fight();
+  //Log a resulting message to the console so we know that it worked. 
+  console.log(playerName + " attacked " + enemyName + ". " + enemyName + " now has "+ enemyHealth + " health remaining."); 
+  
+
+  //Subtract the value of 'enemyAttack' from the value of 'playerHealth 
+  //and use that result to update the value in the 'playerHealth' variable. 
+
+  //Log a resulting message to the console so we know that it worked. 
+}; 
+
+fight(); 
 
 // window.alert("This is an alert! JavaScript is running!"); 
 // // Game States
